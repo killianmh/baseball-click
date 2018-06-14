1418f3bea9d591185da29b7927aae832a8d22a5b
killianmh <killianmh@gmail.com>
2018-06-14 14:00:21 -0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import pics from './pics.json';
+import Header from './components/Header';
+import Jumbo from './components/Jumbo';
+import Wrapper from './components/Wrapper';
+import PicCard from './components/PicCard';
+import Footer from './components/Footer';
+import './App.css'
 
-class App extends Component {
+class App extends React.Component {
+  state={
+    pics,
+    chosen: [],
+    message: "Click an image to begin!",
+    score: 0,
+    topscore: 0,
+    winner: false
+  }
+
+  handleClick = (event, id) => {
+    event.preventDefault();
+    console.log(id)
+    let newPics =[];
+
+    if(this.state.chosen.includes(id)){
+      this.setState(function(prevState, props){
+        newPics = prevState.pics;
+        for(let i = prevState.pics.length - 1; i > 0; i--){
+          const j = Math.floor(Math.random() * (i + 1));
+          let temp = newPics[i];
+          newPics[i] = newPics[j];
+          newPics[j] = temp;
+        }
+        console.log(prevState.pics)
+        console.log(newPics)
+        return({
+          pics: newPics,
+          chosen: [],
+          message: "Wrong Pick!",
+          score: 0
+        })
+      })
+    };
+
+    if(!this.state.chosen.includes(id)){
+      console.log(this.state.pics);
+      this.setState(function(prevState, props){
+        newPics = prevState.pics;
+        for(let i = prevState.pics.length - 1; i > 0; i--){
+          const j = Math.floor(Math.random() * (i + 1));
+          let temp = newPics[i];
+          newPics[i] = newPics[j];
+          newPics[j] = temp;
+        }
+        console.log(newPics)
+        console.log(prevState.chosen);
+        let newChosen = prevState.chosen.slice();
+        newChosen.push(id);
+        console.log(newChosen);
+        const newScore = prevState.score + 1;
+        let newTopscore;
+
+        if(newScore === 12){
+          newTopscore = newScore;
+          return({
+            pics: newPics,
+            chosen: [],
+            message: "You Win!",
+            score: 0,
+            topscore: newTopscore,
+            winner: true
+          })
+        }
+
+        else {
+          if(prevState.topscore < newScore){
+            newTopscore = newScore;
+          }
+          else{
+            newTopscore = prevState.topscore
+          }
+  
+          return({
+            pics: newPics,
+            chosen: newChosen,
+            message: "Correct! Keep it up!",
+            score: newScore,
+            topscore: newTopscore
+          })
+        }
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Container">
+        <Header 
+          message = {this.state.message}
+          score = {this.state.score}
+          topscore = {this.state.topscore}
+        />
+        <Jumbo />
+        <div className="PicCard-display">
+          <Wrapper>
+            {this.state.pics.map(pic => {
+              return(
+                <a
+                  key= {pic.id}
+                  onClick={(event) => this.handleClick(event, pic.id)}>
+                    <PicCard
+                      image = {pic.image}
+                    />
+                </a>
+              )
+            })}
+          </Wrapper>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => {
+    return(
+    <nav className="navbar nav-justified">
+        <a className="navbar-brand nav-item pg-title" href="https://killianmh.github.io/clickyGame/">Click-It!</a>
+        <span className="navbar-brand nav-item msg"> { props.message }</span>
+        <span className="navbar-brand nav-item score"> {`Score: ${props.score} | Top Score: ${props.topscore}`}</span>
+    </nav>
+    )
+}
+
+export default Header;
